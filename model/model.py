9e79806c2734032c294ca5b4b7f8319cdf4d1a52
Abel <se.abel.tadesse@gmail.com>
2025-12-28 17:23:18 +0300

    Normalization Layer

--- model/model.py
+++ model/model.py
@@ -49,3 +49,19 @@ class PositionalEncoding(nn.Module):
         x = self.droupout(x)    
 
         return x
+    
+class LayerNormalization(nn.Module):
+    
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__ =()
+        self.eps = eps
+        
+        self.alpha = nn.Parameter(torch.ones((1,))) # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # Added
+        
+    def forward(self, x):
+        mean = x.mean(dim = -1 , keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * ( x - mean) / (std + self.eps) + self.bias
+    
+    
