b6d4fd8fa6b0e89658ba73e08ee82bf6df1c4cb2
Abel <se.abel.tadesse@gmail.com>
2025-12-28 17:01:47 +0300

    Input Embeddings and Positional Encoding

---
+++ model/model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import math
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model:int, vocab_size: int):
+        super().__init()
+        self.d_model = d_model
+        self.vocab.size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        
+    
+    def forward(self,x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+    
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int , dropout: float ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.droupout = nn.Dropout(dropout)
+        
+        
+        # Create a Matrix of shape (seq_len, d_model)
+        
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape
+        
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        
+        # Apply the sin to even positions
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+       
+        pe = pe.unsqueeze(0)  # Shape: (1, seq_len, d_model)
+        self.register_buffer('pe', pe)
+        
+    
+    def forward(self, x):
+        x = x + (self.pe[:, :x.size(1), :]).requires_grad_(False)
+        x = self.droupout(x)    
+
+        return x
