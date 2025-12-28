a4ce5817ff5214da3f2af7a5713ae1e81cfa3266
Abel <se.abel.tadesse@gmail.com>
2025-12-29 00:36:26 +0300

    decoder block

--- model/model.py
+++ model/model.py
@@ -161,3 +161,17 @@ class Encoder(nn.Module):
                 
             return self.norm(x)
         
+class DecoderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+        
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        x = self.residual_connection[1](x, lambda x: self.cross_attention_block(x, enc_output, enc_output, src_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
