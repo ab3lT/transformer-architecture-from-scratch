8cda5e4650e6b5f38f3dd86cdb7cf85d8849f9fa
Abel <se.abel.tadesse@gmail.com>
2025-12-29 00:31:25 +0300

    Encoder

--- model/model.py
+++ model/model.py
@@ -147,3 +147,17 @@ class EncoderBlock(nn.module):
             x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,mask))
             x = self.residual_connection[1](x, self.feed_forward_block)
             return x
+
+class Encoder(nn.Module):
+    
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+        
+        def forward(self,x,mask):
+            for layer in self.layers:
+                x = layer(x, mask)
+                
+            return self.norm(x)
+        
